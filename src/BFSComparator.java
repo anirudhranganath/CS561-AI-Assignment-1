dc03e3b03ece0aa7d1238597e200203215322ac5
Anirudh Ranganath <anipub@hotmail.com>
2013-02-20 23:26:46 -0800

    Added BFS and DFS Comparators

---
+++ src/BFSComparator.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: anirudh
+ * Date: 20/2/13
+ * Time: 7:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Comparator;
+
+public class BFSComparator implements Comparator<Location>{
+        @Override public int compare(Location x, Location y) {
+           return 1;
+        }
+}
