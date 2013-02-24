98a51a495ddf198f9213e7eefc108a4f4ff0f33c
Anirudh Ranganath <anipub@hotmail.com>
2013-02-24 12:43:55 -0800

    added new config options and removed irrelevant tests

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+    public static float doubleEpsilon = 0.01f;
+    public static float decrementalSpeedReductionOnMud = 0.1f;
+    public static Algorithm algoinUse = Algorithm.BFS;
     public static void main(String[] args) throws IOException{
         // Testing input
         Problem x = new Problem();
-        x.loadInputFromFile("my_sample_input.txt");
-        Solver s = new Solver(x,Algorithm.BFS);
+        x.loadInputFromFile("input_example.txt");
+        Solver s = new Solver(x,algoinUse);
         s.solve();
         Logger.outputLogInfo("out.txt");
-
-
-        /*testing comparators
-        Comparator<Location> comparator = new DFSComparator();
-        PriorityQueue<Location> container =
-                new PriorityQueue<Location>(10, comparator);
-        container.add(new Location(3,4));
-        container.add(new Location(4,5));
-        for(Location x:container){
-            System.out.println(container.poll().xcood);
-            System.out.println(container.poll().xcood);
-            if(container.poll()==null) {
-                System.out.println("cool");
-            }
-        }*/
     }
 }
